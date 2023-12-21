7afba2982adfe1ace1d2270084d61de89ab10546
hsani2344 <hkm.sani@pm.me>
2023-12-21 10:44:14 +0100

    Update: finance.db

--- Part 01 - CS50/Week 09 - Flask/finance/query.sql
+++ Part 01 - CS50/Week 09 - Flask/finance/query.sql
@@ -14,13 +14,13 @@
 -- SET cash = 89.13
 -- WHERE id = 1;
 
--- CREATE TABLE "stocks" (
---     id INTEGER PRIMARY KEY UNIQUE NOT NULL,
---     user_id SERIAL NOT NULL,
---     symbol CHAR(15) NOT NULL,
---     shares INT UNSIGNED NOT NULL DEFAULT 1,
---     FOREIGN KEY (user_id) REFERENCES users(id)
--- );
+CREATE TABLE "stocks" (
+    id INTEGER PRIMARY KEY UNIQUE NOT NULL,
+    user_id SERIAL NOT NULL,
+    symbol CHAR(15) NOT NULL,
+    shares INT UNSIGNED NOT NULL DEFAULT 1,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
 
 -- INSERT INTO stocks(user_id, symbol)
 -- VALUES (1, 'NKE');
@@ -33,6 +33,6 @@
 -- SELECT * FROM stocks
  -- WHERE user_id = 1;
 
- UPDATE users SET cash = 300 WHERE id = 1;
+ -- UPDATE users SET cash = 300 WHERE id = 1;
 
 
